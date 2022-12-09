1f51385618c6495386727d0d9f1ee7fda89a1205
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-12-09 22:30:35 +0200

    Add Components, Redux, .github, License, babelrc and all files needed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cities from './Components/City';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import Pollution from './Components/Pollution';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/city" element={<Cities />} />
+        <Route path="/pollutiondata" element={<Pollution />} />
+      </Routes>
+    </Router>
   );
 }
 
