7f33dfdba0359ba132c342143ffdffb57a0ea4e4
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-12-09 23:55:32 +0200

    Fix linter errors

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../Redux/configureStore';
+import store from '../Redux/ConfigureStore';
 import Home from '../Components/Home';
 
 it('Home', () => {
